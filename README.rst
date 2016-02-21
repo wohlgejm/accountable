edf7bda7db5743c2c833eab1b2dbf3e885df39d3
jerry <wohlgejm@gmail.com>
2016-02-21 14:42:36 -0500

    Fix ups to tests using tmp directory

--- README.rst
+++ README.rst
@@ -7,13 +7,16 @@ Accountable - a Jira CLI (Alpha)
 
 Never leave the command line to update a ticket again!
 
-## Getting started
+Getting started
+===============
+To install:
+`pip install accountable`
 
-## Why?
+
+Why?
+===
 Jira already supports robus triggers, like changing a ticket's status
 based on a pull request, or branch created. However, there are times
 when these automated triggers aren't enough.
 
-Often, you'll start work
-locally, and forget to put the ticket in progress. You
-ever leave the command line to update a ticket again!
+Often, you'll start work locally, and forget to put the ticket in progress.
--- tests/test_accountable.py
+++ tests/test_accountable.py
@@ -1,4 +1,5 @@
 import mock
+import pytest
 
 from accountable.accountable import Accountable
 
@@ -12,24 +13,37 @@ def config_values():
     }
 
 
+@pytest.fixture
+def accountable(tmpdir, **kwargs):
+    Accountable.CONFIG_DIR = '{}/.accountable'.format(str(tmpdir))
+    Accountable.CONFIG_FILE = '{}/config.yaml'.format(Accountable.CONFIG_DIR)
+    return Accountable(**kwargs)
+
+
 class TestAccountable(object):
+    def test_creates_config_file(self, tmpdir):
+        accountable(tmpdir, **config_values())
+        assert len(tmpdir.listdir()) == 1
+
+    @mock.patch.object(Accountable, '_load_config')
     @mock.patch.object(Accountable, '_initial_setup')
-    def test_initial_setup(self, mock_method, tmpdir):
-        Accountable.CONFIG_DIR = str(tmpdir)
-        Accountable(**config_values())
+    def test_initial_setup(self, mock_method, mock_load, tmpdir):
+        accountable(tmpdir, **config_values())
         mock_method.assert_called_once_with()
 
     @mock.patch.object(Accountable, '_load_config')
     def test_load_config(self, mock_method, tmpdir):
-        Accountable.CONFIG_DIR = str(tmpdir)
-        Accountable(**config_values())
+        accountable(tmpdir, **config_values())
         mock_method.assert_called_once_with()
 
+    @mock.patch.object(Accountable, '_load_config')
     @mock.patch.object(Accountable, '_initial_setup')
-    def test_does_not_setup_if_config_exists(self, mock_method, tmpdir):
-        Accountable.CONFIG_DIR = str(tmpdir)
-        Accountable(**config_values())
+    def test_does_not_setup_if_create_not_specified(self,
+                                                    mock_method,
+                                                    mock_load,
+                                                    tmpdir):
+        accountable(tmpdir, **config_values())
         do_not_create_config = config_values().copy()
         del do_not_create_config['create_config']
-        Accountable(**do_not_create_config)
+        accountable(tmpdir, **do_not_create_config)
         mock_method.assert_called_once_with()
