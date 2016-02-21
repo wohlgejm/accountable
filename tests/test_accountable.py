27c9857a715ca34c6dd7139a4a8253575c4f45d4
jerry <wohlgejm@gmail.com>
2016-02-21 14:08:43 -0500

    Use py.test tmpdir

--- tests/test_accountable.py
+++ tests/test_accountable.py
@@ -14,17 +14,20 @@ def config_values():
 
 class TestAccountable(object):
     @mock.patch.object(Accountable, '_initial_setup')
-    def test_initial_setup(self, mock_method):
+    def test_initial_setup(self, mock_method, tmpdir):
+        Accountable.CONFIG_DIR = str(tmpdir)
         Accountable(**config_values())
         mock_method.assert_called_once_with()
 
     @mock.patch.object(Accountable, '_load_config')
-    def test_load_config(self, mock_method):
+    def test_load_config(self, mock_method, tmpdir):
+        Accountable.CONFIG_DIR = str(tmpdir)
         Accountable(**config_values())
         mock_method.assert_called_once_with()
 
     @mock.patch.object(Accountable, '_initial_setup')
-    def test_does_not_setup_if_config_exists(self, mock_method):
+    def test_does_not_setup_if_config_exists(self, mock_method, tmpdir):
+        Accountable.CONFIG_DIR = str(tmpdir)
         Accountable(**config_values())
         do_not_create_config = config_values().copy()
         del do_not_create_config['create_config']
