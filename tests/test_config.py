85f8274ed97c7acd4d82b0c3f281fa33a78a7558
jerry <wohlgejm@gmail.com>
2016-03-13 21:51:58 -0400

    Test fixup

--- tests/test_config.py
+++ tests/test_config.py
@@ -13,7 +13,7 @@ class TestConfig(object):
     @mock.patch.object(Config, '_initial_setup')
     def test_initial_setup(self, mock_method, mock_load, tmpdir):
         config(tmpdir, **config_values())
-        mock_method.assert_called_once_with()
+        mock_method.assert_called_once_with(config_values())
 
     @mock.patch.object(Config, '_load_config')
     def test_load_config(self, mock_method, tmpdir):
@@ -22,12 +22,12 @@ class TestConfig(object):
 
     @mock.patch.object(Config, '_load_config')
     @mock.patch.object(Config, '_initial_setup')
-    def test_does_not_setup_if_create_not_specified(self,
-                                                    mock_method,
-                                                    mock_load,
-                                                    tmpdir):
+    def test_does_not_setup(self,
+                            mock_method,
+                            mock_load,
+                            tmpdir):
         config(tmpdir, **config_values())
         do_not_create_config = config_values().copy()
         del do_not_create_config['create_config']
         config(tmpdir, **do_not_create_config)
-        mock_method.assert_called_once_with()
+        mock_load.called_once_with(**config_values())
