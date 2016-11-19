572df8fa702313a3f5f75d2b0ae967a19c6b108e
wohlgejm <wohlgejm@gmail.com>
2016-11-19 08:40:16 -0500

    Use doubles for stubbing requests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 
 from click.testing import CliRunner
 import mock
+import requests
+from doubles import allow
 
 from accountable import cli
 from tests import support
@@ -25,9 +27,8 @@ def test_issuetypes(mock_object):
     assert result.output == '1 - AC - Bug - An error in the code\n'
 
 
-@mock.patch('requests.get')
-def test_issue(mock_object):
-    mock_object.return_value = support.issue()
+def test_issue():
+    allow(requests).get.and_return(support.issue())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101'])
     assert result.exit_code == 0
@@ -37,9 +38,8 @@ def test_issue(mock_object):
                              'DESCRIPTION - example bug report\n')
 
 
-@mock.patch('requests.get')
-def test_issue_comments(mock_object):
-    mock_object.return_value = support.comments()
+def test_issue_comments():
+    allow(requests).get.and_return(support.comments())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'comments'])
     assert result.exit_code == 0
@@ -47,20 +47,18 @@ def test_issue_comments(mock_object):
                              '2016-05-18T12:19:03.615+0000\n')
 
 
-@mock.patch('requests.get')
-def test_issue_no_comments(mock_object):
+def test_issue_no_comments():
     response = support.MockResponse(200)
     response.data = {}
-    mock_object.return_value = response
+    allow(requests).get.and_return(response)
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'comments'])
     assert result.exit_code == 0
     assert result.output == 'No comments found for DEV-101\n'
 
 
-@mock.patch('requests.post')
-def test_addcomment(mock_object):
-    mock_object.return_value = support.comment()
+def test_addcomment():
+    allow(requests).post.and_return(support.comment())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'addcomment',
                                      'A comment'])
@@ -69,9 +67,8 @@ def test_addcomment(mock_object):
                              '2016-05-18T12:19:03.615+0000\n')
 
 
-@mock.patch('requests.get')
-def test_issue_worklog(mock_object):
-    mock_object.return_value = support.issue_worklog()
+def test_issue_worklog():
+    allow(requests).get.and_return(support.issue_worklog())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'worklog'])
     assert result.exit_code == 0
@@ -80,42 +77,38 @@ def test_issue_worklog(mock_object):
                              'Time spent - 3h 20m\n')
 
 
-@mock.patch('requests.get')
-def test_issue_no_worklog(mock_object):
+def test_issue_no_worklog():
     response = support.MockResponse(200)
     response.data = {}
-    mock_object.return_value = response
+    allow(requests).get.and_return(response)
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'worklog'])
     assert result.exit_code == 0
     assert result.output == 'No worklogs found for DEV-101\n'
 
 
-@mock.patch('requests.get')
-def test_issue_transitions(mock_object):
-    mock_object.return_value = support.issue_transitions()
+def test_issue_transitions():
+    allow(requests).get.and_return(support.issue_transitions())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'transitions'])
     assert result.exit_code == 0
     assert result.output == '2 - Close Issue\n711 - QA Review\n'
 
 
-@mock.patch('requests.get')
-def test_issue_no_transitions(mock_object):
+def test_issue_no_transitions():
     response = support.MockResponse(200)
     response.data = {}
-    mock_object.return_value = response
+    allow(requests).get.and_return(response)
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101', 'transitions'])
     assert result.exit_code == 0
     assert result.output == 'No transitions found for DEV-101\n'
 
 
-@mock.patch('requests.post')
-def test_do_transition(mock_post):
+def test_do_transition():
     transition = support.MockResponse(204)
     transition.data = float('-inf')
-    mock_post.return_value = transition
+    allow(requests).post.and_return(transition)
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['issue', 'DEV-101',
                                      'dotransition', str(1)])
@@ -123,9 +116,8 @@ def test_do_transition(mock_post):
     assert result.output == 'Successfully transitioned DEV-101\n'
 
 
-@mock.patch('requests.post')
-def test_createissue_nargs(mock_post):
-    mock_post.return_value = support.issue_create()
+def test_createissue_nargs():
+    allow(requests).post.and_return(support.issue_create())
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['createissue', 'project.id', '1'])
     assert result.exit_code == 0
@@ -135,10 +127,9 @@ def test_createissue_nargs(mock_post):
 
 
 @mock.patch('accountable.accountable.Accountable._repo')
-@mock.patch('requests.post')
-def test_checkoutbranch(mock_post, mock_repo):
+def test_checkoutbranch(mock_repo):
+    allow(requests).post.and_return(support.issue_create())
     mock_repo.return_value = support.MockRepo()
-    mock_post.return_value = support.issue_create()
     runner = CliRunner()
     result = runner.invoke(cli.cli,
                            ['checkoutbranch', 'project.id', '1',
@@ -150,10 +141,9 @@ def test_checkoutbranch(mock_post, mock_repo):
 
 
 @mock.patch('accountable.accountable.Accountable._repo')
-@mock.patch('requests.get')
-def test_checkout(mock_get, mock_repo):
+def test_checkout(mock_repo):
+    allow(requests).get.and_return(support.issue())
     mock_repo.return_value = support.MockRepo()
-    mock_get.return_value = support.issue()
     runner = CliRunner()
     result = runner.invoke(cli.cli, ['checkout', 'TST-24'])
     assert result.exit_code == 0
@@ -163,10 +153,9 @@ def test_checkout(mock_get, mock_repo):
 
 
 @mock.patch('accountable.accountable.Accountable._repo')
-@mock.patch('requests.post')
-def test_cob(mock_post, mock_repo):
+def test_cob(mock_repo):
+    allow(requests).post.and_return(support.issue_create())
     mock_repo.return_value = support.MockRepo()
-    mock_post.return_value = support.issue_create()
     runner = CliRunner()
     result = runner.invoke(cli.cli,
                            ['cob', 'project.id', '1', 'summary', 'slug me'])
@@ -177,12 +166,11 @@ def test_cob(mock_post, mock_repo):
 
 
 @mock.patch('accountable.accountable.Accountable._repo')
-@mock.patch('requests.post')
 @mock.patch('accountable.accountable.Accountable.aliases')
-def test_custom_alias(mock_aliases, mock_post, mock_repo):
+def test_custom_alias(mock_aliases, mock_repo):
+    allow(requests).post.and_return(support.issue_create())
     mock_aliases.return_value = {'custom': 'checkoutbranch'}
     mock_repo.return_value = support.MockRepo()
-    mock_post.return_value = support.issue_create()
     runner = CliRunner()
     result = runner.invoke(cli.cli,
                            ['custom', 'project.id', '1', 'summary', 'slug me'])
@@ -193,10 +181,9 @@ def test_custom_alias(mock_aliases, mock_post, mock_repo):
 
 
 @mock.patch('accountable.accountable.Accountable._repo')
-@mock.patch('requests.post')
-def test_alias_not_found(mock_post, mock_repo):
+def test_alias_not_found(mock_repo):
+    allow(requests).post.and_return(support.issue_create())
     mock_repo.return_value = support.MockRepo()
-    mock_post.return_value = support.issue_create()
     runner = CliRunner()
     result = runner.invoke(cli.cli,
                            ['notthere', 'project.id', '1', 'summary',
@@ -206,9 +193,8 @@ def test_alias_not_found(mock_post, mock_repo):
                             'Error: No such command "notthere".\n'
 
 
-@mock.patch('requests.get')
-def test_users(mock_get):
-    mock_get.return_value = support.users()
+def test_users():
+    allow(requests).get.and_return(support.users())
     runner = CliRunner()
     result = runner.invoke(cli.cli,
                            ['users', 'e'])
