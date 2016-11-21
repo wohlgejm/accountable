aff2d7056e75ff671a341ddd03843d3e6f681397
wohlgejm <wohlgejm@gmail.com>
2016-11-20 20:32:06 -0500

    Break resource into own module

--- accountable/accountable.py
+++ accountable/accountable.py
@@ -9,6 +9,7 @@ from git import Repo
 from slugify import slugify
 
 from accountable.config import Config
+from accountable.resource import Resource
 
 
 class Accountable(object):
@@ -149,25 +150,3 @@ class Accountable(object):
             return field.upper()
         else:
             return list(field.keys())[0].upper()
-
-
-class Resource(object):
-    def __init__(self):
-        self.config = Config()
-        self.auth = self.config.auth
-        self.api_uri = 'https://{}/rest/api/2'.format(self.config['domain'])
-
-    def get(self, resource, params={}):
-        r = requests.get('{}/{}'.format(self.api_uri, resource),
-                         params=params,
-                         auth=self.auth)
-        return r.json()
-
-    def post(self, resource, payload={}):
-        r = requests.post('{}/{}'.format(self.api_uri, resource),
-                          auth=self.auth,
-                          json=payload)
-        try:
-            return r.json()
-        except ValueError:
-            return r
---
+++ accountable/resource.py
@@ -0,0 +1,25 @@
+import requests
+
+from accountable.config import Config
+
+
+class Resource(object):
+    def __init__(self):
+        self.config = Config()
+        self.auth = self.config.auth
+        self.api_uri = 'https://{}/rest/api/2'.format(self.config['domain'])
+
+    def get(self, resource, params={}):
+        r = requests.get('{}/{}'.format(self.api_uri, resource),
+                         params=params,
+                         auth=self.auth)
+        return r.json()
+
+    def post(self, resource, payload={}):
+        r = requests.post('{}/{}'.format(self.api_uri, resource),
+                          auth=self.auth,
+                          json=payload)
+        try:
+            return r.json()
+        except ValueError:
+            return r
